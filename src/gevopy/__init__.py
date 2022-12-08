15be395e8ca53e3963ba513db78a6da9e23e8e9b
Borja Esteban <boressan@outlook.com>
2022-12-08 21:49:23 +0000

    second draft for experiments module

--- src/gevopy/__init__.py
+++ src/gevopy/__init__.py
@@ -35,49 +35,21 @@ class Experiment():
     Provides the essential attributes to create and run an experiment.
     """
 
-    def __init__(self, population, fitness, name=None, database=None):
-        """Constructor base for a evolution experiments.
-        :param population: List of phenotypes
-        :param fitness: Fitness instance to evaluate phenotypes
-        :param name: Experiment name, if none, generates an uuid4 string
-        :param database: Neo4j database connection driver, defaults to None
-        """
-        self.__logg = logging.getLogger(f"{__name__}.Experiment")
-        self.__name = name if name else str(uuid.uuid4())
-        self.__logg = self.Logger(self.logger, {"name": self.name})
-        self.__phenotypes = []
-        self.fitness = fitness
-        self.database = database
-        self.add_phenotypes(population)
-
     class Logger(logging.LoggerAdapter):
         """Adds `Experiment.name` to the logs if available."""
 
         def process(self, msg, kwargs):
             return f"[{(self.extra['name'], msg)}] {kwargs}"
 
-    @property
-    def database(self):
-        """Neo4j driver used to interface the database.
-        :return: Neo4j.Driver instance or None
+    def __init__(self, fitness, name=None, database=EmptyInterface()):
+        """Constructor base for a evolution experiments.
+        :param fitness: Fitness instance to evaluate phenotypes
+        :param name: Experiment name, if none, generates an uuid4 string
+        :param database: Database interface object, defaults to EmptyInterface 
         """
-        if isinstance(self.__db, Neo4jInterface):
-            return self.__db.driver
-        elif isinstance(self.__db, EmptyInterface):
-            return None
-        else:
-            raise RuntimeError(f"Unexpected value:'{self.__db}' for database")
-
-    @database.setter
-    def database(self, value):
-        if isinstance(value, neo4j.Driver):
-            self.logger.info("Enabled neo4j graph database feature")
-            self.__db = Neo4jInterface(value)
-        elif value is None:
-            self.logger.info("Disabled neo4j graph database feature")
-            self.__db = Neo4jInterface(value)
-        else:
-            raise ValueError("Expected 'neo4j.Driver' for 'database'")
+        self.name = name if name else str(uuid.uuid4())
+        self.fitness = fitness
+        self.database = database
 
     @property
     def name(self):
@@ -85,6 +57,25 @@ class Experiment():
         :return: string
         """
         return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__logg = logging.getLogger(f"{__name__}.Experiment")
+        self.__logg = self.Logger(self.logger, {"name": value})
+        self.__name = f"{value}"
+
+    @property
+    def database(self):
+        """Database interface used to write and read from database.
+        :return: gevopy.database object instance or None
+        """
+        return self.__db
+
+    @database.setter
+    def database(self, value):
+        if not isinstance(value, (Neo4jInterface, EmptyInterface)):
+            raise ValueError("Expected 'gevopy.database' interface")
+        self.__db = value
 
     @property
     def logger(self):
@@ -106,48 +97,95 @@ class Experiment():
             raise ValueError("Expected 'gevopy.FitnessModel' instance")
         self.__fitness = value
 
-    def add_phenotypes(self, phenotypes):
-        """Adds phenotypes to the experiment and the database (if driver).
-        :param phenotypes: List of phenotypes to add to the experiment
-        """
-        if not isinstance(phenotypes, list) or phenotypes == []:
-            raise ValueError("Expected non empty 'list' for 'phenotypes'")
-        if any(not isinstance(x, genetics.GenotypeModel) for x in phenotypes):
-            raise ValueError("Phenotypes must inherit from GenotypeModel")
-
-        self.logger.debug("Adding phenotypes:\n%s", phenotypes)
-        for phenotype in phenotypes:  # Add experiment to phenotypes
-            phenotype.experiment = self.name
-        self.__db.add_phenotypes(phenotypes)
-        self.__phenotypes += phenotypes
-
-    def get_phenotypes(self, serialize=False):
-        """Returns an iterable with all the intern experiment phenotypes.
-        :param serialize: If phenotypes should be returned as serialized dict
-        :return: Iterable of experiment phenotypes
-        """
-        if serialize:
-            return (p.dict(serialize=True) for p in self.__phenotypes)
-        return iter(self.__phenotypes)
-
     @contextlib.contextmanager
     def session(self, *args, **kwds):
         """Function to generate a context session to interface the experiment.
         :param args: Same as neo4j.Driver session possitional arguments
         :param kwds: Same as neo4j.Driver session key arguments
         """
-        with GraphSession(self.database, *args, **kwds) as session:
+        with self.__db.session(*args, **kwds) as db_session:
             self.logger.debug("New session with:\n%s\n%s", args, kwds)
-            yield session
+            yield Session(experiment=self, database=db_session)
 
-    def run(self, algorithm=DEFAULT_ALGORITHM, **kwds):
-        """Executes the algorithm until a stop condition is met.
-        :param algorithm: Algorithm to run each execution generation cycle
-        :param kwds: Same as gevopy.Execution key arguments
+
+class Session():
+    """Base class for evolution experiment sessions.
+    Provides the essential attributes to prepare the execution conditions.
+    """
+
+    def __init__(self, experiment, database):
+        self.__experiment = experiment
+        self.__population = []
+        self.__db = database
+
+    @property
+    def population(self):
+        """Returns the list of current phenotypes at the session.
+        :return: List of experiment phenotypes
+        """
+        return self.__population
+
+    @property
+    def logger(self):
+        """Session logger, borrowed from the contained experiment.
         :return: Generated Execution instance
         """
-        self.logger.info("Run start for algorithm: %s", algorithm)
-        return Execution(**kwds).run(self, algorithm)
+        return self.__experiment.logger
+
+    def save_phenotypes(self, phenotypes):
+        """Saves the phenotypes to the experiment database.
+        :param phenotypes: List of phenotypes to add to the experiment
+        """
+        if any(not isinstance(x, genetics.GenotypeModel) for x in phenotypes):
+            raise ValueError("Phenotypes must inherit from GenotypeModel")
+        iserial_phenotypes = (p.dict(serialize=True) for p in phenotypes)
+        self.__db.add_phenotypes(iserial_phenotypes)  # Iterator to speed up
+
+    def run(self, **execution_kwds):
+        """Executes the algorithm until a stop condition is met.
+        :param max_generation: The maximum number of loops to run
+        :param max_score: The score required to stop the evolution
+        :return: Generated Execution instance
+        """
+        self.logger.info("Run start for experiment session")
+        return Execution(**execution_kwds).run(session=self)
+
+    def add_phenotypes(self, phenotypes, save=True):
+        """Adds phenotypes to the experiment and the database.
+        :param phenotypes: List of phenotypes to add to the experiment
+        :param save: Flag to save new population status in database
+        """
+        if not isinstance(phenotypes, list) or phenotypes == []:
+            raise ValueError("Expected non empty 'list' for 'phenotypes'")
+        if any(not isinstance(x, genetics.GenotypeModel) for x in phenotypes):
+            raise ValueError("Phenotypes must inherit from GenotypeModel")
+        for phenotype in phenotypes:  # Add experiment to phenotypes
+            phenotype.experiment = self.__experiment.name
+        if save:
+            self.save_phenotypes(phenotypes)
+        self.__population += phenotypes
+
+    def eval_phenotypes(self, fitness, save=True):
+        """Executes the fitness evaluation on the session population.
+        :param algorithm: Algorithm to run each execution generation cycle
+        :param save: Flag to save new population status in database
+        """
+        if not isinstance(fitness, FitnessModel):
+            raise ValueError("Expected 'FitnessModel' type for 'fitness'")
+        fitness(self.__population)
+        if save:
+            self.save_phenotypes(self.__population)
+
+    def generate_offspring(self, algorithm, save=True):
+        """Replaces population with offspring generated from algorithm.
+        :param algorithm: Algorithm to run for producing the offspring
+        :param save: Flag to save new population status in database
+        """
+        if not isinstance(algorithm, algorithms.Algorithm):
+            raise ValueError("Expected 'Algorithm' type for 'algorithm'")
+        self.__population = algorithm(self.__population)
+        if save:
+            self.save_phenotypes(self.__population)
 
 
 class Execution():
@@ -161,32 +199,15 @@ class Execution():
 
     def __init__(self, max_generation=None, max_score=None):
         """Constructor base for a evolutionary algorithm execution.
-        :param session: Experiment session used to run the exolution
         :param max_generation: The maximum number of loops to run
         :param max_score: The score required to stop the evolution
         """
         if not max_generation and not max_score:
             raise ValueError("Required valid end conditions for Execution")
-
-        self.__logg = logging.getLogger(f"{__name__}.Execution")
-        self.__halloffame = tools.HallOfFame(3)
-        self.__generation = 0
+        self._halloffame = tools.HallOfFame(3)
+        self._generation = 0
         self.max_generation = max_generation
         self.max_score = max_score
-
-    @property
-    def logger(self):
-        """Experiment logger, used to trace and print experiment info.
-        :return: Experiment.Logger
-        """
-        return self.__logg
-
-    @property
-    def generation(self):
-        """Number of generation the execution has run.
-        :return: Integer higher or equal to 0
-        """
-        return self.__generation
 
     @property
     def max_generation(self):
@@ -204,16 +225,6 @@ class Execution():
         self.__max_generation = value
 
     @property
-    def best_score(self):
-        """Best score reached by the evaluated phenotypes during the run.
-        :return: Float (not only positive)
-        """
-        try:  # If not started, halloffame is empty and raises IndexError
-            return self.__halloffame[0]
-        except IndexError:  # Empty if not started
-            return None
-
-    @property
     def max_score(self):
         """Configured maximum score to reach by phenotypes before the stop.
         :return: Float (not only positive)
@@ -226,86 +237,53 @@ class Execution():
             raise ValueError("Expected 'float', 'int' or None for max score")
         self.__max_score = value
 
+    @property
+    def best_score(self):
+        """Best score reached by the evaluated phenotypes during the run.
+        :return: Float (not only positive)
+        """
+        try:  # If not started, halloffame is empty and raises IndexError
+            return self._halloffame[0]
+        except IndexError:  # Empty if not started
+            return None
+
     class RunLogger(logging.LoggerAdapter):
         """Adds `Experiment.name` and `generation` to the logs."""
 
         def process(self, msg, kwargs):
             return f"[{(self.extra['exp'], self.extra['gen'], msg)}] {kwargs}"
 
-    def run(self, experiment, algorithm):
+    def run(self, session):
         """Executes the algorithm until a stop condition is met.
-        :param experiment: Experiment used for the evolution execution
-        :param algorithm: Algorithm to run each execution generation cycle
+        :param session: Experiment session used for the evolution execution
         :return: Completed Execution instance (statistics)
         """
-        if not isinstance(experiment, Experiment):
-            raise ValueError("Expected 'Experiment' type for 'experiment'")
-
-        logger_extra = dict(exp=experiment.name, gen=self.generation)
-        run_logger = self.RunLogger(self.logger, logger_extra)
-
+        if not isinstance(session, Session):
+            raise ValueError("Expected 'Session' type for 'session'")
+        logger = self.RunLogger(session.logger, dict(gen=self._generation))
+        fitness, algorithm = session.fitness, session.algorithm
         try:
-            resource = acquire_resource(*args, **kwds)
-
-        finally:  # Code to release resource:
-            if experiment.database:
-                session = stack.enter_context(session)
-            else:
-                session = None
-
-        with contextlib.ExitStack() as stack:
-            if experiment.database:
-                session = stack.enter_context(session)
-            else:
-                session = None
-            try:
-                run_logger.info("Start of evolutionary experiment execution")
-                self.eval(session, experiment)
-                experiment.eval_phenotypes()  # Evaluate offspring phenotypes
-                if experiment.database:  # If db available save phenotypes
-                    session.add_phenotypes(experiment.get_phenotypes())
-                while offspring := self.cycle(session, experiment, algorithm):
-                    run_logger.debug("Cycle offspring: %s", offspring)
-                    run_logger.info(self.best_score)
-                    if experiment.database:  # If db available save phenotypes
-                        session.add_phenotypes(experiment.get_phenotypes())
-            except Exception as error:
-                run_logger.info("Error raised during experiment execution")
-                raise error
-            else:
-                run_logger.info("Experiment execution completed successfully")
-                return self
-
-    def cycle(self, session, experiment, algorithm):
-        """Spawns and evaluates genotype generations with the input algorithm.
-        :param session: Experiment session opened during evolution runtime
-        :param experiment: Evolutionary experiment with evolution properties
-        :param algorithm: Evolutionary algorithm to apply on each generation
-        :return: Offspring if experiment is not completed, None otherwise
-        """
-        self.__generation += 1  # Increase generation index for the execution
-        experiment.generate_offspring(algorithm)  # Replace pop with offspring
-        return self.eval(session, experiment)
-
-    def eval(self, session, experiment):
-        """Set up the execution process of the experiment evolution algorithm.
-        :param session: Experiment session opened during evolution runtime
-        :param experiment: Evolutionary experiment with evolution properties
-        :return: Offspring if experiment is not completed, None otherwise
-        """
-        experiment.eval_phenotypes()  # Evaluate offspring phenotypes
-        self.__halloffame.update(experiment.get_phenotypes(), self.generation)
-        if experiment.database:
-        session.add_phenotypes(experiment.get_phenotypes(serialize=True))
-
-        return experiment.get_phenotypes() if not self.completed else None
+            logger.info("Start of evolutionary experiment execution")
+            session.eval_phenotypes(fitness, save=True)  # Evaluate first pop
+            while not self.completed:
+                logger.info("New execution cycle; %s", self.best_score)
+                self._generation += 1  # Increase generation index
+                session.generate_offspring(algorithm, save=False)
+                session.eval_phenotypes(fitness, save=True)
+                self._halloffame.update(session.population, self._generation)
+        except Exception as error:
+            logger.info("Error raised during experiment execution")
+            raise error
+        else:
+            logger.info("Experiment execution completed successfully")
+            return self
 
     @property
     def completed(self):
         """Evaluates if the final generation or required score is reached.
         :return: True if evolution conditions are met, False otherwise
         """
-        if self.max_generation and self.max_generation <= self.generation:
+        if self.max_generation and self.max_generation <= self._generation:
             return True
         if self.max_score and self.max_score <= self.best_score:
             return True
@@ -317,7 +295,7 @@ class Execution():
         """
         return (
             "- Evolutionary algorithm execution report -----\n"
-            f"\tExecuted generations:\t{self.generation}\n"
-            f"\tBest phenotype:\t{self.__halloffame[0].id}"
+            f"\tExecuted generations:\t{self._generation}\n"
+            f"\tBest phenotype:\t{self._halloffame[0].id}"
             f"\tBest score:\t{self.best_score}\n"
         )
